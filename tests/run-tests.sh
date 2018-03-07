d5e1951519fe6193635890f059dbb9b496841291
William L. Thomson Jr <wlt@o-sinc.com>
2018-03-06 22:41:19 -0500

    tests: Create test files on first invocation of run-tests.sh

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -8,8 +8,29 @@ VG="${VG} --show-reachable=yes --track-origins=yes --error-exitcode=1"
 BIN_TEST="$(find .  | grep 'dist/asspr-test')"
 BIN="${BIN_TEST%%-*}"
 
+ASSP="build/var/lib/assp/"
+CONFIG="build/etc/assp/"
+
 check_rc() {
 	[[ ${1} -ne 0 ]] && exit "${1}"
+}
+
+init_files() {
+	local email emails ext exts
+
+	emails=( abuse admin mailer-daemon postmaster root webmaster )
+	exts=( com net org )
+	if [[ ! -d "${ASSP}" ]] && [[ ! -d "${CONFIG}" ]]; then
+		mkdir -p "${ASSP}"/{discarded,notspam,spam} "${CONFIG}"
+
+		for ext in "${exts[@]}"; do
+			echo "domain.${ext}" >> "${CONFIG}/localdomains.txt"
+			for email in "${emails[@]}"; do
+				echo "${email}@domain.${ext}" \
+					>> "${CONFIG}/localaddresses.txt"
+			done
+		done
+	fi
 }
 
 test_code() {
@@ -18,7 +39,9 @@ test_code() {
 }
 
 test_bin() {
-	${VG} "${BIN}" -a tests/var/lib/assp/ -C tests/etc/assp/ -c -s
+	init_files
+
+	${VG} "${BIN}" -a "${ASSP}" -C "${CONFIG}" -c -s
 }
 
 case "$1" in
